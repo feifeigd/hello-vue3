066b541821864966bebdff60177f0c632992570c
feifeigd <feifeigd@21cn.com>
2020-09-24 18:26:39 +0800

    使用自定义指令 v-focus

---
+++ src/components/Directive.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input v-focus>
+    </div>
+    <div>支持多个根标签了</div>
+</template>
+
+<script lang="ts">
+import {Vue} from 'vue-class-component'
+export default class extends Vue{
+    
+}
+</script>
