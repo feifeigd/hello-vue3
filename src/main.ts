af84a98e69ac5ddb2fa7a24970a81b54e25b82fa
feifeigd <feifeigd@21cn.com>
2020-09-24 13:38:27 +0800

    注册全局自定义指令

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 注册全局自定义指令
+// v-focus
+app.directive('focus', {    
+    mounted(el) {
+        // Focus the element
+        el.focus();
+    } 
+});
+app.mount('#app');
