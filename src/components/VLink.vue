58aab0ca26892d83dbc3a78f68386aa8b76d8480
feifeigd <feifeigd@21cn.com>
2020-09-25 13:12:42 +0800

    路由

---
+++ src/components/VLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <a :href="href" @click.prevent="go">
+      <slot></slot>
+  </a>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import routes from '../routes';
+
+@Options({
+    props: ['href', ],
+})
+export default class extends Vue{
+    href!:string;
+    
+    go(){
+        console.log('go', this.href);
+        let root = this.$root as any;
+        if(root)root.currentRoute = this.href;
+        window.history.pushState(null, routes[this.href], this.href);
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/layouts/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+      <li v-for="(value, path) in routes" :key="path"><v-link :href="path">{{value}}</v-link></li>
+  </ul>
+  <slot />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import VLink from '../components/VLink.vue';
+import routes from '../routes';
+
+@Options({
+    components:{
+        VLink,
+    }
+})
+export default class extends Vue{
+    routes = routes;
+    mounted(){
+        console.log(routes);
+    }
+}
+</script>
+
+<style>
+
+</style>
