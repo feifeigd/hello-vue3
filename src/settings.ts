c03909691bf303c693a3b6026a8b51c087d986f3
feifeigd <feifeigd@21cn.com>
2024-03-05 14:29:32 +0800

    defaultSettings

---
+++ src/settings.ts
@@ -0,0 +1,18 @@
+
+const defaultSettings = {
+    fixedHeader: false,
+    title: 'Vue3 Admin Template',
+    language: 'zh-cn',
+    layout: 'left',
+    showSettings: true,
+    sidebarLogo: true,
+    size: 'default',
+    tagsView: true,
+    theme: "left",
+    themeColor: "#409EFF",
+    version: 'v1.0.0',
+    watermarkEnabled: false,
+    watermarkContent: 'Vue3 Admin Template',
+};
+
+export default defaultSettings;
