779260ff9b1bf44155b06e97663fcc4a99ac1e4b
feifeigd <feifeigd@21cn.com>
2024-03-05 14:19:08 +0800

    ThemeEnum

---
+++ src/enums/ThemeEnum.ts
@@ -0,0 +1,19 @@
+
+/**
+ * 主题枚举
+ */
+export enum ThemeEnum {
+    /**
+     * 暗黑主题
+     */
+    DARK = 'dark',
+    /**
+     * 明亮主题
+     */
+    LIGHT = 'light',
+
+    /**
+     * 系统自动
+     */
+    AUTO = 'auto',
+}
