fe3583af966dfb58bddb19aa491c105025be1129
feifeigd <feifeigd@21cn.com>
2024-03-06 17:25:39 +0800

    svg icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,41 @@
+<template>
+    <svg
+        class="svg-icon"
+        aria-hidden="true"
+        :style="{width: size, height: size}"
+    >
+        <use :xlink:href="symbolId" :fill="color" />
+    </svg>
+    svg icon
+</template>
+
+<script lang="ts">
+import { Prop } from 'vue-facing-decorator';
+
+@Component
+class SvgIcon extends Vue {
+    @Prop iconClass = "";
+    @Prop size = "1em";
+    @Prop color = "";
+    @Prop prefix = "icon";
+
+    get symbolId() {
+        return `#${this.prefix}-${this.iconClass}`;
+    }
+}
+export default toNative(SvgIcon);
+</script>
+
+<style lang="scss" scoped>
+    .svg-icon {
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+
+        vertical-align: -0.15em;    // 因icon大小被设置为和字体大小一致，而span等标签的下边缘会和字体的基线对齐，故需设置一个往下的偏移比例，来纠正视觉上的未对齐效果
+        fill: currentColor; // 定义元素的颜色，currentColor是一个变量，这个变量的值就表示当前元素的color值，如果当前元素未设置color值，则从父元素继承
+
+        outline: none;
+        overflow: hidden;
+    }
+</style>
