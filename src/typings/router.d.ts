b9a7fc8c33ac350d6a84cfe492c6d31255d6acc6
feifeigd <feifeigd@21cn.com>
2024-03-07 15:45:58 +0800

    RouteMeta

---
+++ src/typings/router.d.ts
@@ -0,0 +1,27 @@
+
+import 'vue-router';    // 这个不能少
+
+declare module 'vue-router' {
+    // https://router.vuejs.org/zh/guide/advanced/meta.html#typescript
+    // 可以通过扩展 RouteMeta 接口来输入 meta 字段
+    interface RouteMeta {
+        // 菜单名称
+        title?: string;
+        // 菜单图标
+        icon?: string;
+        noCache: boolean;
+        // 是否固定在 tagsView
+        affix?: boolean;
+        // 是否在面包屑上隐藏
+        breadcrumb?: boolean;
+        activeMenu: string;
+        // 是否隐藏
+        hidden?: boolean;
+        // 是否缓存页面
+        keepAlive?: boolean;
+        // 拥有菜单权限的角色编码
+        roles?: string[];
+    }
+}
+
+export {};
