e513ef00791a9797edc3c3f4e316c85b4f615135
feifeigd <feifeigd@21cn.com>
2024-03-11 15:11:16 +0800

    hamburger

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,32 @@
+<template>
+    <div @click="toggle" class="px-[15px] flex items-center justify-center color-[var(--el-text-color-regular)]">
+        <SvgIcon class="hamburger" icon-class="indent-decrease" :class="{'is-active': isActive}" />
+    </div>
+</template>
+
+<script lang="ts">
+import {  Prop, Emit } from 'vue-facing-decorator';
+
+@Component
+class Hamburger extends Vue {
+    @Prop({ default: false,  required: true, type: Boolean, }) readonly isActive!: boolean;
+
+    @Emit
+    toggle() {
+        return this.isActive;
+    }
+}
+export default toNative(Hamburger);
+</script>
+
+<style scoped lang="scss">
+.hamburger {
+    cursor: pointer;
+    transform: scaleX(-1);
+    vertical-align: middle;
+
+}
+.hamburger.is-active {
+    transform: scaleX(1);
+}
+</style>
