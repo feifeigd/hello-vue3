0b24a5241b1b14ecf4296675b4b6149e869142a3
feifeigd <feifeigd@21cn.com>
2024-03-14 11:41:58 +0800

    navbar left

---
+++ src/layout/components/NavBar/components/NavbarLeft.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex">
+        <Hamburger :is-active="appStore.sidebar.opened" @toggle="appStore.toggleSidebar" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useAppStore } from '@/store';
+
+const appStore = useAppStore();
+
+</script>
